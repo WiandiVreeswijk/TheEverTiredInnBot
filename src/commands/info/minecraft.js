0453dfe36edcb514072427bf349dc9557847ddd2
DESKTOP-E0GU1RS\Wia <wiavre@gmail.com>
2026-02-23 07:52:51 +0100

    fixed indentation in minecraft info command

--- src/commands/info/minecraft.js
+++ src/commands/info/minecraft.js
@@ -14,13 +14,13 @@ module.exports = {
                 .setDescription(
                     'This server runs on **Minecraft Bedrock Edition**.\n\n' +
                     '**You can join from:**\n' +
-                    'Windows, Xbox, PlayStation, Switch, iOS, Android'
+                    'Windows, Xbox, PlayStation, Switch, iOS, Android\n\n' +
+                    'The server requires an invite before you can join. Please tag @illy in the #🪵minecraft-server or DM him with your username and you will be added to the server playerlist.'
                 )
                 .addFields(
                     { name: '🌐 IP', value: '`152.228.198.219`', inline: true },
                     { name: '🔌 Port', value: '`19132`', inline: true }
                 )
-                .setDescription('The server requires an invite before you can join. Please tag @illy in the #🪵minecraft-server or DM him with your username and you will be added to the server playerlist.')
                 .setTimestamp();
 
             const guideEmbed = new EmbedBuilder()

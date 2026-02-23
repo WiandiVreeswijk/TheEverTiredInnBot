96e7162d8efc02115e54f8fe8f3d6367bda7a171
DESKTOP-E0GU1RS\Wia <wiavre@gmail.com>
2026-02-23 18:46:53 +0100

    fixed pathing again

--- src/handlers/interactionHandler.js
+++ src/handlers/interactionHandler.js
@@ -117,7 +117,7 @@ module.exports = async (client, interaction) => {
             if (interaction.customId === 'fireside_prompt') {
 
                 const pool = require('../database/db')
-                const prompts = require('../fireside/firesidePrompts')
+                const prompts = require('../commands/fireside/firesidePrompts')
 
                 const { rows } = await pool.query(`
                     SELECT *

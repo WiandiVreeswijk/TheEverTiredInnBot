33c8b5f8295fb16c3af8be18ea0b51a61d604100
Wiandi Vreeswijk <wiavre@gmail.com>
2026-07-04 22:05:03 +0200

    Added Rione topics

---
+++ src/utils/gifTopics.js
@@ -0,0 +1,61 @@
+module.exports = [
+
+    "Current Status",
+
+    "My Personal Hell",
+
+    "Trying to Be Productive",
+
+    "If I Won the Lottery",
+
+    "Me After Making One Good Decision",
+
+    "When Someone Says 'Trust Me'",
+
+    "My 3 A.M. Thoughts",
+
+    "Checking My Bank Account",
+
+    "Back-to-Work Mood",
+
+    "Worst Person to Sit Beside on a Plane",
+
+    "When Someone Says 'We Need to Talk'",
+
+    "Describe Your Day",
+
+    "When the Food Arrives",
+
+    "Biggest Red Flag",
+
+    "If My Life Were a TV Show",
+
+    "My Guilty Pleasure",
+
+    "Me on a First Date",
+
+    "If I Had to Fight 100 Ducks",
+
+    "When the Wi-Fi Dies",
+
+    "Rage Quitting",
+
+    "Pretending I Know What's Going On",
+
+    "Monday Morning",
+
+    "My Witness Protection Identity",
+
+    "Trying to Act Normal",
+
+    "If I Were Arrested...",
+
+    "My Toxic Trait",
+
+    "How I'd Survive the Apocalypse",
+
+    "After One Inconvenience",
+
+    "If I Were Running the Country"
+
+]; 
